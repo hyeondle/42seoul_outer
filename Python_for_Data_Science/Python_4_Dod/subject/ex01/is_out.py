8c81137000514b45be8f60f4a8c58e20677fa854
hyeondle <hyeondle@users.noreply.github.com>
2025-03-14 14:40:48 +0900

    add prototypes

---
+++ Python_for_Data_Science/Python_4_Dod/subject/ex01/is_out.py
@@ -0,0 +1,10 @@
+def square(x: int | float) -> int | float:
+    # code
+
+def pow(x: int | float) -> int | float:
+    # code
+
+def outer(x: int | float, function) -> object:
+        count = 0
+    def inner() -> float:
+        # code
